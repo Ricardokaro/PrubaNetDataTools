0b7992e2a0eae8c56f85cf117c0c622afab74fdc
Ricardo Caro <ricardo.caro@gmail.com>
2022-10-20 21:04:53 -0500

    Agregar archivos de proyecto.

---
+++ PrubaNetDataTools/Cliente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrubaNetDataTools
+{
+    public class Cliente
+    {
+        public string? nombre { get; set; }
+
+        public int cantidad { get; set; }
+
+        public double valor { get; set; }
+
+
+    }
+}
---
+++ PrubaNetDataTools/Program.cs
@@ -0,0 +1,34 @@
+﻿using PrubaNetDataTools;
+using System.Text.Json;
+
+double leerResultadoJson()
+{   
+    string? rutaArchivo = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\datos.json");
+
+    string jsonString = File.ReadAllText(rutaArchivo);
+    
+    Cliente? cliente = JsonSerializer.Deserialize<Cliente>(jsonString);
+    
+    double rts = 0;
+    
+    if (cliente == null)
+    {
+        rts = 0;
+    }
+    else
+    {
+        rts = cliente.cantidad * cliente.valor;
+    }
+
+    return rts;
+}
+
+double rst = leerResultadoJson();
+
+Console.WriteLine($"Valor: ${ rst }");
+
+
+
+
+
+
